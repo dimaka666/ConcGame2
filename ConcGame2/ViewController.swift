46380193e7c065a5feebfccae8ae4b2b54cd38fd
dima <dmitrimitikov@gmail.com>
2018-04-18 17:16:07 +0300

    Lesson 1

--- ConcGame2/ViewController.swift
+++ ConcGame2/ViewController.swift
@@ -9,18 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0 {
+        didSet {
+           flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices: Array<String> = ["😈","👹","💩","🤖","😈","👹","💩","🤖"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            print("Card Number = \(cardNumber)")
+            cardFlip(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Not in a list")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func cardFlip(withEmoji emoji: String, on button: UIButton) {
+        print("cardFlip(withEmoji: \(emoji))")
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
     }
-
-
+    
 }
 
